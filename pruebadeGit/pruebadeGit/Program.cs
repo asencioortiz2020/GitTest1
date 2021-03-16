b0b0260a5ab1f450a56be7d74c66fe81c14dbecb
asencioortiz2020 <61093048+asencioortiz2020@users.noreply.github.com>
2021-03-16 16:22:26 +0100

    Update Program.cs

--- pruebadeGit/pruebadeGit/Program.cs
+++ pruebadeGit/pruebadeGit/Program.cs
@@ -11,7 +11,31 @@ namespace pruebadeGit
             //ASEORRR
             //CODIGO DESDE vs
             //codigo a llamar clase
+            //remote repository fetch/working repository pull/index(cache)add/local repository commit//remote repo--push
+            //fetch pul push,todos se encuentran en VS
+            //responsable-responsables-repository-desallador(dictador-tenientes...responsables de subsistemas)
+            //repositorio-delladores(estilo subversion-sist.centralizado)
+            //repositorio-responsable-delladores(resposnsable-integracion...encargado de actualizar repo)
+            //tortoise ankhsvn visualsvn (todos son clientes subversion VS)
+            //Centralizado(CVS,Subversion)
+            //Distribuido(Git,Mercurial,Baazar)
+            //proyecto subversion:trunk(almacena proyecto principal),tag(etiqueta versiones estables del proyecto),
+            //branches(permite crear alternativas del proyecto sin alterar el proyecto principal
+            //repositrio(lugar donde se almacenan los datos actualizados-historicos
+            //branch(permitir dllo fichero a diferentes velocidades
+            //revision(version determinada de archivo-proyecto
+            //commit(integrar cambios al repositorio
+            //no esta relacionado con control versiones(SandCastle fhb)
+            //en sis.distribuido (cada dllador tiene repo local
+            //extension VisualSvn tienes que instalar softwre para funcionar
+            //Subversion(arquitec.cliente-servidor,,dllado por Apache año 2000,software libre
+            //flujo distribuido existen responsables de subsistemas-->dictador-tenientes
+            //no relacionado con sist.centralizado (git)
+            
+            
+            
             //fetch-recuperar(traes los cambios del remoto) // pull-extraer(ves los cambios del remoto) //
+            
         }
     }
 }
