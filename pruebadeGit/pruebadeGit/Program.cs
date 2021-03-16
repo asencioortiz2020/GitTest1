8b400fbc53100afd11366751d0c162ac7360c34b
asencioortiz2020 <61093048+asencioortiz2020@users.noreply.github.com>
2021-03-16 16:51:24 +0100

    Update Program.cs

--- pruebadeGit/pruebadeGit/Program.cs
+++ pruebadeGit/pruebadeGit/Program.cs
@@ -31,10 +31,37 @@ namespace pruebadeGit
             //Subversion(arquitec.cliente-servidor,,dllado por Apache año 2000,software libre
             //flujo distribuido existen responsables de subsistemas-->dictador-tenientes
             //no relacionado con sist.centralizado (git)
+            //descomentar ctrl+k
+            
+            
+            //errors compilacion(cuando aparece en rojo, errores escritos)
+            //errores en tiempo ejecucion(cuando no se puede aparece una excepcion)
+            //errores logicos(compila bien pero no tiene sentido)
+            //depuracion (automatico..muestra variables en linea actual//Inspeccion..puedes agregar variables)
+            
             
             
             
             //fetch-recuperar(traes los cambios del remoto) // pull-extraer(ves los cambios del remoto) //
+            //incoming commits-confirmaciones de entrada(lo que tienes pendiente desde github)
+            //outgoing commits-salida(pendiente de lo q ahs hecho en VS)
+            
+            //team explorer-->cambios-->añades program.cs(boton dcho)stage(o lo q quieres que se suba sin todos los archivos)
+            //haces commit->cambios,symc,insertar/push) ahora el proyecto está localmente y en github
+            //hago cambios en github,Vs no sabe q hay cambios,por eso  hago fetch,trae cambios pero no los veo
+            //voy a ramas-->mergefusion mediante comb...merge from(origin/master en rama actual),
+            //fusiona rama master remoto cn master local,ahora lo ves
+            //hago cambio en github,en VS hago pull y aparece codigo nuevo
+            //ramas--boton dcho(nueva rama local ...,commit,sync,push..en github ves commits si cambias de rama
+            //fusiono rama2 con master-->pull request-->merge ,confirm
+            //en ramaMaster githb ok pero en VS no lo sabe,sincronizo mi master en VS
+            //voy a master en VS sync,extraer/pull,ahora nuevo codigo en VS
+            //si hay conflictos-->ves a conflicto(doble click)comparar archivos,take source/editado en Github
+            //keep target/conservar lo de VS
+            
+            //hago fork y lo clono,hago pull request
+            
+            
             
         }
     }
